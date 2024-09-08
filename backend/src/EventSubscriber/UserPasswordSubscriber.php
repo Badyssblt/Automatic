6c13bf0fb06796bd724166e14ca80c24bc155eb2
Badyss <badyss.blt@gmail.com>
2024-09-08 09:16:39 +0200

    Feat: Register/Login with JWT

---
+++ backend/src/EventSubscriber/UserPasswordSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Symfony\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+class UserPasswordSubscriber implements EventSubscriberInterface
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::VIEW => ['hashPassword', EventPriorities::PRE_WRITE],
+        ];
+    }
+
+    public function hashPassword(ViewEvent $event): void
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$user instanceof User || !in_array($method, ['POST', 'PUT'])) {
+            return;
+        }
+
+        // Hash the password
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hashedPassword);
+    }
+}
